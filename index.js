67b2dae2b93cee23b34a9d60a7e47ecf907fa8da
Michael Cleary <mcleary@Michaels-MacBook-Pro.local>
2017-03-30 23:07:45 -0700

    add level progress tracker

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function levelProgress() {
+  if (window.offset <= 0) {
+    return "0 %"
+  } else {
+    return Math.floor(window.offset / 3780) + " %";
+  }
+};
+// var p = document.querySelector("#levelProgress")
+// p.addEventListener("change", function() {
+//
+// });
+
 document.querySelector("#levelCount").textContent = (window.level);
+document.querySelector("#lifeCount").textContent = (window.lives);
+document.querySelector("#levelProgress").textContent = levelProgress();
